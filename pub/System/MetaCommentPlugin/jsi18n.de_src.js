35c0f204c3795bb4889ca909d7dc23ee439fcf86
Jan Krüger <krueger@modell-aachen.de>
2016-05-20 11:36:07 +0200

    Fix typo in German blockUI message for updating comments
    Ticket: none
    Release-Note: none

--- pub/System/MetaCommentPlugin/jsi18n.de_src.js
+++ pub/System/MetaCommentPlugin/jsi18n.de_src.js
@@ -1,6 +1,6 @@
 jsi18n.add('MetaCommentPlugin', {
     "Submitting comment ...": "Übermittle Kommentar...",
-    "Updating comment [_1] ...": "Aktuallisiere Kommentar [_1]...",
+    "Updating comment [_1] ...": "Aktualisiere Kommentar [_1]...",
     "Deleting comment [_1] ...": "Lösche Kommentar [_1] ...",
     "Approving comment [_1] ...": "Gebe Kommentar [_1] frei...",
     "": ""
